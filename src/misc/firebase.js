5cc70e36ac6d6fa200d4f575b064339088df658e
sauryBhai <saurabhsharma200301@gmail.com>
2022-01-17 14:05:52 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyDdpw2J8woU5kQVieeIL3gilypU-t5hob0",
+    authDomain: "chat-app-49705.firebaseapp.com",
+    projectId: "chat-app-49705",
+    storageBucket: "chat-app-49705.appspot.com",
+    messagingSenderId: "685040804044",
+    appId: "1:685040804044:web:d35997704b7d265c6ae269"
+  };
+
+  const app = firebase.initializeApp(config);
