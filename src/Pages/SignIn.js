4db3f904186fba5f1cc79feb4a517104f2beb6de
sauryBhai <saurabhsharma200301@gmail.com>
2022-01-18 22:46:11 +0530

    added sign in page with facebook and google

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import firebase from 'firebase/app';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite'
+import { auth, database } from '../misc/firebase';
 
 export default function SignIn() {
+
+   const SignInWithProvider = async (provider) =>{
+      
+      try {
+        const {additionalUserInfo, user} =await auth.signInWithPopup(provider);
+
+        if(additionalUserInfo.isNewUser)
+        {
+            await database.ref(`/profile/${user.uid}`).set({
+                name:user.displayName,
+                createdAt : firebase.database.ServerValue.TIMESTAMP
+            })
+        }
+        Alert.success('Signed In',4000);
+      } catch (err) {
+          Alert.info(err.message,4000);
+      }
+      
+   };
+
+
+   const onFacebookSignIn =()=>{
+    SignInWithProvider( new firebase.auth.FacebookAuthProvider());
+   };
+
+   const onGoogleSignIn =()=>{
+    SignInWithProvider( new firebase.auth.GoogleAuthProvider());
+   };
     return (
-        <div>
-            Sign in
-            
-        </div>
+        <Container>
+
+            <Grid className='mt-page'>
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                        <Panel>
+                            <div className='text-center'>
+                                <h2>Welcome to Chat</h2>
+                                <p>Progressive Chat platform for neophytes</p>
+                            </div>
+                            <div className='mt-3'>
+                                <Button block color='blue' onClick={onFacebookSignIn}>
+                                   <Icon icon="facebook "/> Continue with Facebook
+                                </Button>
+                                <Button block color='red' onClick={onGoogleSignIn}>
+                                    <Icon icon="google"/> Continue with Google
+                                </Button>
+                            </div>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+
+        </Container>
     )
 }
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,11 @@
 import firebase from "firebase/app";
+import 'firebase/auth'
+import  'firebase/database'
 
 const config = {
     apiKey: "AIzaSyDdpw2J8woU5kQVieeIL3gilypU-t5hob0",
     authDomain: "chat-app-49705.firebaseapp.com",
+    databaseURL: "https://chat-app-49705-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "chat-app-49705",
     storageBucket: "chat-app-49705.appspot.com",
     messagingSenderId: "685040804044",
@@ -10,3 +13,5 @@ const config = {
   };
 
   const app = firebase.initializeApp(config);
+  export const auth = app.auth();
+  export const database = app.database();
