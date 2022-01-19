13145d9438abc67ebec13708f8a761e7894ad597
sauryBhai <saurabhsharma200301@gmail.com>
2022-01-19 20:47:46 +0530

    manage user with context API Created profileContext

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Switch } from 'react-router-dom'
 import 'rsuite/dist/styles/rsuite-default.css';
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
+import { ProfileProvider } from "./context/profile.context";
 import Home from "./Pages/Home";
 import SignIn from "./Pages/SignIn";
 import './styles/main.scss'
 
 function App() {
   return (
+   
     <BrowserRouter>
+     <ProfileProvider>
       <Switch>
         <PublicRoute exact path="/signin">
          <SignIn/>
@@ -18,6 +21,7 @@ function App() {
           <Home />
         </PrivateRoute>
       </Switch>
+      </ProfileProvider>
     </BrowserRouter>
   );
 }
---
+++ src/context/profile.context.js
@@ -0,0 +1,64 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable arrow-body-style */
+import { React, createContext, useState, useContext, useEffect } from "react";
+import { auth, database } from "../misc/firebase";
+
+const profileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+    const [profile,setProfile] =useState(null);
+    const [isLoading ,setIsLoading] =useState(true);
+    
+    useEffect(()=>{
+
+        let UserRef;
+
+        const authUnSub = auth.onAuthStateChanged(authObj=>{
+
+            if(authObj){
+                 
+                UserRef=database.ref(`/profile/${authObj.uid}`);
+                // when any data will be changed this on function will be called which will call, callBack function
+                UserRef.on('value',(snap)=>{
+                      const {name, createdAt} = snap.val();
+
+                      const data ={
+                        name,
+                        createdAt,
+                        uid :authObj.uid,
+                        email :authObj.email
+                      }
+                    setProfile(data);
+                    setIsLoading(false);
+                });
+        
+            }else{
+
+                // this statement will run when we sign off this will unmount the object UserRef
+
+                if(UserRef){
+                    UserRef.off();
+                }
+
+                setProfile(null);
+                setIsLoading(false);
+            }
+        })
+
+        // this will run when we have to unsubscribe the login
+        return ()=>{
+            authUnSub();
+            if(UserRef){
+                UserRef.off();
+            }
+        }
+    },[])
+
+
+    return (
+         <profileContext.Provider value={{ isLoading, profile}}>
+             {children}
+        </profileContext.Provider>);
+};
+
+export const useProfile =()=> useContext(profileContext);
