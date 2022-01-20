715a5b7e1bcf406c885cbb9fb73e0264a565a8ff
sauryBhai <saurabhsharma200301@gmail.com>
2022-01-20 22:34:57 +0530

    added useMedia hook

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useCallback,useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 
 
 
@@ -14,3 +14,22 @@ export function useModalState(defaultValue=false){
 
 // this is a use defined hook which set the state of the dashboard if true it is open else close 
 // used useState hook
+
+export const useMediaQuery = query => {
+    const [matches, setMatches] = useState(
+      () => window.matchMedia(query).matches
+    );
+  
+    useEffect(() => {
+      const queryList = window.matchMedia(query);
+      setMatches(queryList.matches);
+  
+      const listener = evt => setMatches(evt.matches);
+  
+      queryList.addListener(listener);
+      return () => queryList.removeListener(listener);
+    }, [query]);
+  
+    return matches;
+  };
+//   this useMediaQuery is a hook to run dashboard responsive 
