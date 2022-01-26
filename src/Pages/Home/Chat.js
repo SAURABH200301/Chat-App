e0cd376ffd8c618610764a7f160e425fae39c03d
sauryBhai <saurabhsharma200301@gmail.com>
2022-01-26 20:35:10 +0530

    started to build chat top; created room descritpion modal

--- src/Pages/Home/Chat.js
+++ src/Pages/Home/Chat.js
@@ -4,6 +4,7 @@ import { Loader } from 'rsuite';
 import Bottom from '../../components/chat-window/bottom';
 import Messages from '../../components/chat-window/messages';
 import Top from '../../components/chat-window/top';
+import { CurrentRoomProvide } from '../../context/current-room.context';
 import { useRooms } from '../../context/Room.context';
 
 export default function Chat() {
@@ -16,12 +17,19 @@ export default function Chat() {
         <Loader center vertical size="md" content="Loading" speed='slow'/>
     }
 
-    const currentRoom = rooms.find(room=> room.id === chatId);
+    const currentRoom = rooms.find(room=> room.Id === chatId);
 
     if(!currentRoom){
         return <h6 className='text-center mt-page'> Chat {chatId} not found</h6>
     }
-  return <>
+
+   const {name,description}=currentRoom;
+
+    const currentRoomData={
+        name,
+        description
+    }
+  return <CurrentRoomProvide data={currentRoomData}>
       <div className='chat-top'>
          <Top/>
        </div>
@@ -31,5 +39,5 @@ export default function Chat() {
        <div className='chat-middle'>
          <Bottom/>
        </div>
-  </>;
+  </CurrentRoomProvide>;
 }
